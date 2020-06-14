3b4ea48e09e944a2d7cbca6af75c280f3abc8996
lestariIrwan <66860926+lestariIrwan@users.noreply.github.com>
2020-06-15 00:56:43 +0700

    Create  ScanNer.java

---
+++ Input output/ScanNer.java
@@ -0,0 +1,30 @@
+/ *
+ * Untuk mengubah header lisensi ini, pilih Header Lisensi di Project Properties.
+* Untuk mengubah file templat ini, pilih Alat | Templat
+ * dan buka templat di editor.
+ * /
+paket  InputOutput ;
+
+import  java.util.Scanner ;
+
+/ **
+ *
+* @author HP
+ * /
+ ScanNer kelas  publik {
+    public  static  kekosongan  main ( String [] args ) {
+        Scanner masukan =  baru  pemindai ( Sistem . Di);
+
+        Nama string , nim, prodi, fak;
+
+        Sistem . keluar . println ( " Nama: " );
+        name = input . nextLine ();
+        Sistem . keluar . println ( " NIM: " );
+        nim = input . nextLine ();
+        Sistem . keluar . println ( " Prodi: " );
+        prodi = input . nextLine ();
+        Sistem . keluar . println ( " Fakultas: " );
+        fak = input . nextLine ();
+    }
+
+}
