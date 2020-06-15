2a7e88c21c21883115d9dc4fe932f3c9317c4347
lestariIrwan <66860926+lestariIrwan@users.noreply.github.com>
2020-06-15 10:19:06 +0700

    Update ScanNer.java

--- Input output/ScanNer.java
+++ Input output/ScanNer.java
@@ -1,26 +1,26 @@
 
-paket  InputOutput ;
+package InputOutput;
 
-import  java.util.Scanner ;
+import java.util.Scanner;
 
-/ **
+/**
  *
-* @author HP
- * /
- ScanNer kelas  publik {
-    public  static  kekosongan  main ( String [] args ) {
-        Scanner masukan =  baru  pemindai ( Sistem . Di);
-
-        Nama string , nim, prodi, fak;
-
-        Sistem . out . println ( " Nama: " );
-        name = input . nextLine ();
-        Sistem . out . println ( " NIM: " );
-        nim = input . nextLine ();
-        Sistem . out . println ( " Prodi: " );
-        prodi = input . nextLine ();
-        Sistem . out . println ( " Fakultas: " );
-        fak = input . nextLine ();
+ * @author HP
+ */
+public class ScanNer {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        
+        String name, nim, prodi, fak;
+        
+        System.out.println("Nama        : " );
+        name = input.nextLine();
+        System.out.println("NIM         : " );
+        nim = input.nextLine();
+        System.out.println("Prodi       : " );
+        prodi = input.nextLine();
+        System.out.println("Fakultas    : " );
+        fak = input.nextLine();
     }
-
+    
 }
