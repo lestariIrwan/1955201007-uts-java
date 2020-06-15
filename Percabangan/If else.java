e5ef3f3bb7a7571e23be2816dc32e67964831e59
lestariIrwan <66860926+lestariIrwan@users.noreply.github.com>
2020-06-15 10:53:56 +0700

    Create If else.java

---
+++ Percabangan/If else.java
@@ -0,0 +1,14 @@
+package Takehome;
+
+/**
+ *
+ * @author HP
+ */
+public class if_else {
+    public static void main(String[] args) {
+        int x=5, y=10;
+        if(x<y){
+            System.out.println("x lebih kecil dari y");
+        }
+    }
+}
