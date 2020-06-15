82b576fe4a8a29853fceb4f4706cf1bda5cd34ec
lestariIrwan <66860926+lestariIrwan@users.noreply.github.com>
2020-06-15 10:13:11 +0700

    Update JO_pane.java

--- Input output/JO_pane.java
+++ Input output/JO_pane.java
@@ -1,38 +1,38 @@
  
-paket  InputOutput ;
+package InputOutput;
 
-import  javax.swing.JOptionPane ;
+import javax.swing.JOptionPane;
 
-/ **
+/**
  *
-* @author HP
- * /
- kelas  publik JO_pane {
-    public  static  kekosongan  main ( String [] args ) {
-        // I / O versi JOption
-
-        String inputNilai =  JOptionPane . showInputDialog ( " input nilai " );
-        int nilai =  Integer . parseInt (inputNilai);
-        if (nilai > =  91  && nilai <=  100 ) {
-            Sistem . keluar . println ( " A " );
-        } lain  jika (nilai > =  86  && nilai <  91 ) {
-            Sistem . keluar . println ( " A- " );
-        } lain  jika (nilai > =  81  && nilai <  86 ) {
-            Sistem . keluar . println ( " B + " );
-        } lain  jika (nilai > =  76  && nilai <  81 ) {
-            Sistem . keluar . println ( " B " );
-        } lain  jika (nilai > =  66  && nilai <  78 ) {
-            Sistem . keluar . println ( " B- " );
-        } lain  jika (nilai > =  56  && nilai <  66 ) {
-            Sistem . keluar . println ( " C " );
-        } lain  jika (nilai > =  45  && nilai <  56 ) {
-            Sistem . keluar . println ( " D " );
-        } lain  jika (nilai > =  0  && nilai <  45 ) {
-            Sistem . keluar . println ( " E " );
-        } lain {
-            Sistem . keluar . println ( " Nilai Salah " );
+ * @author HP
+ */
+public class JO_pane {
+    public static void main(String[] args) {
+        // I/O versi JOption
+        
+        String inputNilai = JOptionPane.showInputDialog("input nilai");
+        int nilai = Integer.parseInt(inputNilai);
+        if (nilai >= 91 && nilai <= 100){
+            System.out.println("A");
+        }else if (nilai >= 86 && nilai < 91){
+            System.out.println("A-");
+        }else if (nilai >= 81 && nilai < 86){
+            System.out.println("B+");
+        }else if (nilai >= 76 && nilai < 81){
+            System.out.println("B");
+        }else if (nilai >= 66 && nilai < 78){
+            System.out.println("B-");
+        }else if (nilai >= 56 && nilai < 66){
+            System.out.println("C");
+        }else if (nilai >= 45 && nilai < 56){
+            System.out.println("D");
+        }else if (nilai >= 0 && nilai < 45){
+            System.out.println("E");
+        }else{
+            System.out.println("Nilai Salah");
         }
-
+        
     }
-
+    
 }
